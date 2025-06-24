51921f2758a74a65490c97aa85d1baeae771baf4
Tinesh Shah <tineshshah87@gmail.com>
2025-06-24 17:05:26 +0200

    impemted the text input and button with animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './Components/InputField';
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+type Person = {
+  name: string;
+  age?: number;
+}
 
-function App() {
+let person: Person = {
+  name: "Tinesh Shah",
+
+}
+
+let printName: (name: string) => number;
+
+function printName1(name: string): number {
+  return 26;
+}
+
+type someobject = {
+  x: number,
+  y: number
+}
+function printCoord(pt: someobject) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 3, y: 7 });
+
+let personName: unknown; //Dont know what it returs.
+
+
+
+
+
+
+
+
+
+
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  // const [name, setName] = useState<string>("Tinesh Shah");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
